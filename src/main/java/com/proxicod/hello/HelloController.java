6b4f6de37ea7b0fc5523092ec169ccd4645fd2e2
El Hadji GUEYE <EHG@partner.local>
2024-10-18 22:54:23 +0200

    add endpoint

--- src/main/java/com/proxicod/hello/HelloController.java
+++ src/main/java/com/proxicod/hello/HelloController.java
@@ -2,6 +2,7 @@ package com.proxicod.hello;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
@@ -16,5 +17,10 @@ public class HelloController {
 	@GetMapping(path = "")
 	public String sayHello() {
 		return "Hello people";
+	}
+	
+	@GetMapping(path = "")
+	public String sayHelloName(@RequestParam String name) {
+		return "Hello "+name;
 	}
 }
